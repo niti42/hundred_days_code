ea6bd6d2d0332fcb021397109b2e657d076adc0f
niti42 <nithishk12@gmail.com>
2023-01-15 18:37:43 +0530

    blind auction game

--- blind_auction/main.py
+++ blind_auction/main.py
@@ -1,11 +1,35 @@
 import os
+from art import logo
 
-os.system('cls')
+def collect_auction_data(name, bid):
+    bid_details = {
+        "name": name,
+        "bidAmount": bid
+    }
+    auction_data.append(bid_details)
+
 
 if __name__ == "__main__":
-    name = input("What is your?: ")
-    bid = int(input("What is your bid?: $"))
-    next_bidder = input("Are there any other bidders? Type 'yes' or 'no'. \n")
-    winner = ''
-    winning_bid = ''
-    print("The winner is {winner} with a bid of ${winning_bid}")
+    print(logo)
+    auction_data = []
+    continue_auction = True
+    while continue_auction:
+        name = input("What is your Name?: ")
+        bid = int(input("What is your bid?: $"))
+        collect_auction_data(name, bid)
+        next_bidder = input("Are there any other bidders? Type 'yes' or 'no'. \n")
+        os.system('cls')
+        if next_bidder == 'no':
+            continue_auction = False
+            print("End of Auction!\n")
+    
+    highest_bidder = {}
+    highest_amount = 0
+    for item in auction_data:
+        bid_amount = item["bidAmount"]
+        if bid_amount > highest_amount:
+            highest_amount = bid_amount
+            highest_bidder = item
+
+    highest_bidder_name = highest_bidder["name"]
+    print(f"The winner is {highest_bidder_name} with a bid of ${highest_amount}")
