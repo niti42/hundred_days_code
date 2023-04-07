0d5a82c5625a0cb86ab450b2228c2495058df6ae
nik12 <nithishk12@gmail.com>
2023-04-08 00:55:07 +0530

    change coffee machine main code to run from that file

--- coffee_machine_oop/main.py
+++ coffee_machine_oop/main.py
@@ -2,22 +2,22 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
-my_cafe = CoffeeMaker()
-coffee_menu = Menu()
-money_handler = MoneyMachine()
-switch = 'on'
-while switch != 'off':
-    response = input(f"what would you like? ({coffee_menu.get_items()}): ")
-    if response == 'off':
-        switch = 'off'
-    elif response == 'report':
-        my_cafe.report()
-        money_handler.report()
-    elif coffee := coffee_menu.find_drink(response):
-        if my_cafe.is_resource_sufficient(coffee):
-            if money_handler.make_payment(coffee.cost):
-                my_cafe.make_coffee(coffee)
+if __name__ == "__main__":
+    my_cafe = CoffeeMaker()
+    coffee_menu = Menu()
+    money_handler = MoneyMachine()
+    switch = 'on'
+    while switch != 'off':
+        response = input(f"what would you like? ({coffee_menu.get_items()}): ")
+        if response == 'off':
+            switch = 'off'
+        elif response == 'report':
+            my_cafe.report()
+            money_handler.report()
+        elif coffee := coffee_menu.find_drink(response):
+            if my_cafe.is_resource_sufficient(coffee):
+                if money_handler.make_payment(coffee.cost):
+                    my_cafe.make_coffee(coffee)
 
 
 
