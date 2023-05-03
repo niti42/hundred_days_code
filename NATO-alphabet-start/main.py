84c456814d649ed93721a304c4be72c03a1e363f
nik12 <nithishk12@gmail.com>
2023-05-03 09:55:59 +0530

    error handling when user enters numbers

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,15 +1,20 @@
 import pandas as pd
-from pprint import pprint
+
+NATO_PHONETIC_ALPHABET = "nato_phonetic_alphabet.csv"
+
+
+def generate_phonetic():
+    nato_df = pd.read_csv(NATO_PHONETIC_ALPHABET)
+    nato_code = {row.letter: row.code for (index, row) in nato_df.iterrows()}
+    word_to_spell = input("Enter a word to get its NATO phonetic code: ").upper()
+    try:
+        word_nato_code = [nato_code[letter] for letter in word_to_spell]
+    except KeyError:
+        print("Sorry, the word to spell must only contain letters in the english alphabet!")
+        generate_phonetic()
+    else:
+        print(word_nato_code)
+
 
 if __name__ == "__main__":
-    nato_df = pd.read_csv("nato_phonetic_alphabet.csv")
-
-    nato_code = {row.letter: row.code for (index, row) in nato_df.iterrows()}
-
-    while True:
-        word_to_spell = input("Enter a word to get its NATO phonetic code: ").upper()
-        if word_to_spell == "EXIT":
-            break
-        word_nato_code = [nato_code[letter] for letter in word_to_spell]
-        print(word_nato_code)
-    print("Exit Program!")
+    generate_phonetic()
